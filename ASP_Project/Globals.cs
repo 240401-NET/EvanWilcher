7fc6d21dc97ed76c828562e423171753a71844e0
Soulem <ewilcher@fullsail.edu>
2024-04-12 23:33:47 -0400

    hiding my credentials

--- ASP_Project/Globals.cs
+++ ASP_Project/Globals.cs
@@ -1,10 +1,12 @@
 using System.Globalization;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 public static class Globals{
     public const string DISPLAYBADNAMEERRORMESSAGE = "Did not find the pokemon, Please ensure the Pokemon's name is correct."; 
     public const string FILETEAMS = "SavedTeamsByTrainers.json";
     public const string FILE = "SavedTeamsByTrainerName.json";
     public const string TESTFILE = "test.json";
+    public const string SQLSERVERCREDENTIALS = "serverCredentials.md";
     private static string dateTimeNow = DateTime.Now.ToString("MM-dd-yy_Hmmss", CultureInfo.InvariantCulture);
     public static string LOGFILE{
        get { return "PKMAppErrorLog_" +  dateTimeNow + ".md"; }
