7de9c6cca541826cff1e358a44f0ee773d2b8654
Soulem <ewilcher@fullsail.edu>
2024-04-15 14:12:37 -0400

    building and running.

--- ASP_Project/Program.cs
+++ ASP_Project/Program.cs
@@ -2,8 +2,11 @@ namespace ASP_Project;
 public class Program{
     static void Main(string[] args){
         WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddControllers();
         WebApplication app = builder.Build();
+        app.UseRouting();
         app.MapGet("/", () => "Hello World!");
+        app.MapControllerRoute(name: "defalt", pattern: "{controller=Trainer}/{action=Index}/");
         app.Run();
     }
 }
